a75b7ec25125f1358661ed82cb5d6da977b2dc55
Sankeerthreddy7376 <117455479+Sankeerthreddy7376@users.noreply.github.com>
2022-12-16 16:56:02 +0530

    3

---
+++ implement_student_class.py
@@ -0,0 +1,25 @@
+class Student:
+    def setName(self,name):
+        self.__name=name
+    def getName(self):
+        return self.__name
+    def setRollNumber(self,rollno):
+        self.__rollno=rollno
+    def getRollNumber(self):
+        return self.__rollno
+student1=Student()
+student2=Student()
+
+#student1
+student1.setName('sankeerth')
+student1.setRollNumber('100')
+
+print(student1.getName())
+print(student1.getRollNumber())
+
+#student2
+student2.setName('vishnu')
+student2.setRollNumber('101')
+
+print(student2.getName())
+print(student2.getRollNumber())
